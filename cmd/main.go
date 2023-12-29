fe958877354580fbab496c800d437d7888cda241
jenovax86 <shruzvn@gmail.com>
2023-12-30 02:56:29 +0330

    feat: add command-line calculator

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,39 @@
 package main
 
+import (
+	"fmt"
+)
 
-func main(){
-	
+func calculater() int {
+	var firstNumber int
+	var secondNumber int
+	var result int
+	var operator string
+
+	fmt.Scan(&firstNumber, &operator, &secondNumber)
+
+	switch operator {
+	case "+":
+		result = firstNumber + secondNumber
+		break
+
+	case "-":
+		result = firstNumber - secondNumber
+		break
+	case "*":
+		result = firstNumber * secondNumber
+		break
+	case "/":
+		result = firstNumber / secondNumber
+		break
+	default:
+		fmt.Println("default")
+	}
+
+	return result
+}
+
+func main() {
+	calculate := calculater()
+	fmt.Println(calculate)
 }
